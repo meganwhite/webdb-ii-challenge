d59c7ab5e65c4684743f98c54fe648625f281d94
Megan White <mfw247@gmail.com>
2019-09-10 14:47:43 -0700

    structured data table

--- data/migrations/20190910143517_createDB.js
+++ data/migrations/20190910143517_createDB.js
@@ -1,8 +1,36 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable("cars", table => {
+        table.increments();
+
+        table
+            .string("vinNumber")
+            .unique()
+            .notNullable()
+
+        table
+            .string("make", 128)
+            .notNullable()
+
+        table
+            .string("model",128)
+            .notNullable()
+        
+        table
+            .integer("mileage")
+            .notNullable()
+
+        table
+            .string("transmissionType", 128)
+        
+        table
+            .string(titleStatus, 128)
+            
+    })
   
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("cars");
 };
+
